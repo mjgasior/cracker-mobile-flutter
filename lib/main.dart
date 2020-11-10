067eec8dab992dfe2231355765214a68d182a951
Michal J. Gasior <michaljgasior@gmail.com>
2020-11-10 19:54:15 +0100

    Step 3: Add a Stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,22 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Cracker'),
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
   }
 }
