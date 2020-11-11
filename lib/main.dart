055e790c1a81b23e1f301184dceb583a0113422a
Michal J. Gasior <michaljgasior@gmail.com>
2020-11-11 22:47:33 +0100

    Adding libs was hard

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class _CrackerAppState extends State<CrackerApp> {
           title: Text('Auth0 Demo'),
         ),
         body: Center(
-          child: Text('Implement User Authentication'),
+          child: Text('Implement User Authentication22'),
         ),
       ),
     );
