7997a73ffbcf9d5700d3e3c0bfcdd66546f3501f
Michal J. Gasior <michaljgasior@gmail.com>
2020-11-15 20:58:30 +0100

    Auth0 and Apollo

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,25 @@ const AUTH0_ISSUER = 'https://$AUTH0_DOMAIN';
 
 void main() => runApp(CrackerApp());
 
+String readRepositories = """
+    {
+    markers {
+      _id
+      latitude
+      longitude
+      imageFilename
+      polish {
+        name
+        description
+      }
+      english {
+        name
+        description
+      }
+    }
+  }
+""";
+
 class CrackerApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final HttpLink httpLink = HttpLink(uri: 'https://cracker.red/api');
@@ -231,7 +250,53 @@ class Login extends StatelessWidget {
           child: Text('Login'),
         ),
         Text(loginError ?? ''),
+        Markers()
       ],
+    );
+  }
+}
+
+class Markers extends StatelessWidget {
+  final _biggerFont = TextStyle(fontSize: 18.0);
+
+  Widget _buildRow(dynamic marker) {
+    return ListTile(
+      title: Text(
+        marker['english']['name'],
+        style: _biggerFont,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Query(
+      options: QueryOptions(
+        documentNode: gql(readRepositories),
+      ),
+      builder: (QueryResult result,
+          {VoidCallback refetch, FetchMore fetchMore}) {
+        if (result.hasException) {
+          return Text(result.exception.toString());
+        }
+
+        if (result.loading) {
+          return Text('Loading');
+        }
+
+        List markers = result.data['markers'];
+
+        return Text(markers[0]['english']['name']);
+
+        /*
+        return ListView.builder(
+            itemCount: markers.length,
+            itemBuilder: (context, index) {
+              final marker = markers[index];
+              return _buildRow(marker);
+            });
+            */
+      },
     );
   }
 }
