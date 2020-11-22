9f7de498fecc570a52aa6e3cbe887974df8bbf44
Michal J. Gasior <michaljgasior@gmail.com>
2020-11-22 20:40:28 +0100

    Read position stream

--- lib/+widgets/marker_tile.dart
+++ lib/+widgets/marker_tile.dart
@@ -1,17 +1,39 @@
 import 'package:cracker_app/+widgets/marker_details.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
-class MarkerTile extends StatelessWidget {
+class MarkerTile extends StatefulWidget {
   final marker;
-  final _biggerFont = TextStyle(fontSize: 18.0);
 
   MarkerTile(this.marker);
 
   @override
+  _MarkerTileState createState() => _MarkerTileState();
+}
+
+class _MarkerTileState extends State<MarkerTile> {
+  final _biggerFont = TextStyle(fontSize: 18.0);
+  Position userLocation;
+
+  @override
+  void initState() {
+    super.initState();
+    print("This is here");
+    Geolocator.getPositionStream(timeInterval: 1000)
+        .listen((Position position) {
+      print(position == null
+          ? 'Unknown'
+          : position.latitude.toString() +
+              ', ' +
+              position.longitude.toString());
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final marker = widget.marker;
     return ListTile(
       onTap: () {
-        print('${marker['polish']['name']} was tapped!');
         Navigator.push(
           context,
           MaterialPageRoute(builder: (context) {
@@ -20,7 +42,11 @@ class MarkerTile extends StatelessWidget {
         );
       },
       title: Text(
-        marker['polish']['name'],
+        marker['english']['name'],
+        style: _biggerFont,
+      ),
+      subtitle: Text(
+        marker['english']['name'],
         style: _biggerFont,
       ),
     );
